492351b966712e41100e185259012d69af6c59ef
Dr-Blank <64108942+Dr-Blank@users.noreply.github.com>
2023-09-29 14:54:30 -0400

    Added SyncedLyricLine

---
+++ tests/test_synced_lyric_line.py
@@ -0,0 +1,28 @@
+import pytest
+
+from lrctoolkit.synced_lyric_line import SyncedLyricLine
+
+
+def test_formatted_lyric_without_timestamp():
+    line = SyncedLyricLine(text="Hello")
+    assert line.formatted_lyric == "Hello"
+
+@pytest.mark.parametrize(
+    "line, expected",
+    [
+        (
+            SyncedLyricLine(text="Hello", timestamp=1000),
+            "[00:01.00]Hello",
+        ),
+        (
+            SyncedLyricLine(text="Hello", timestamp=200001),
+            "[03:20.00]Hello",
+        ),
+        (
+            SyncedLyricLine(text="Hello", timestamp=200111),
+            "[03:20.11]Hello",
+        ),
+    ],
+)
+def test_formatted_lyric_with_timestamp(line: SyncedLyricLine, expected):
+    assert line.formatted_lyric == expected
